4f7457b07e0776e29a4a1867f589191ea0e4b9ea
Yazish Kavina <yazishkavina@gmail.com>
2023-07-26 00:12:05 +0530

    GitHub commit

---
+++ ano.py
@@ -0,0 +1,19 @@
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+import pandas as pd
+import os
+os.environ["OPENAI_API_KEY"] = r"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"
+
+loader = CSVLoader(file_path=r'./dateset.csv')    
+
+# Create an index using the loaded documents
+index_creator = VectorstoreIndexCreator()
+docsearch = index_creator.from_loaders([loader])
+
+chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+query = input("Type the Query  ")
+response = chain({"question": query})
+print(response['result'])
+print("--------------------------------")
